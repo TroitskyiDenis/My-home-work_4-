5a38d7bbd51eefd43aacf1aaa453717d5b52c390
unknown <dtroitskyi@gmail.comgit config --global user.name 'Denis>
2023-07-11 22:02:45 +0300

    Сделано

---
+++ Задача_1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+Console.Clear();
+
+int[] arr = new int[8];
+FillArray(arr,99,999);
+Console.WriteLine(String.Join(" ", arr));
+Console.WriteLine(GetCount(arr));
+void FillArray(int[] arr, int mini, int maxi)
+{
+    for (int i = arr.Length - 1; i >= 0; i--)
+    {
+        arr[i] = new Random().Next(mini, maxi+1);
+    }
+}
+
+int GetCount(int[] arr)
+{
+    int count = 0; 
+   foreach (var item in arr)
+   {
+    if (item%2 == 0)
+    {
+       count++; 
+    }
+   }
+     return  count;
+}
---
+++ Задача_2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+int[] arr = new int[9];
+FillArray(arr,-9,9);
+Console.WriteLine(String.Join(" ", arr));
+Console.WriteLine(GetCount(arr));
+
+void FillArray(int[] arr, int mini, int maxi)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(mini, maxi+1);
+    }
+}
+
+int GetCount(int[] arr)
+{
+    int sum = 0; 
+   for (int i=0; i<arr.Length; i++ )
+   {
+     
+    if (i%2 == 1)
+    {
+       sum+=arr[i];
+    }
+   }
+     return  sum;
+}
